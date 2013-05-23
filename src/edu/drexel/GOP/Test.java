42376bc8d88d95ed21c5741fff45e23ba1296ce8
lasalvavida <rht25@drexel.edu>
2013-05-23 15:21:55 -0400

    Test method now uses multicast

--- src/edu/drexel/GOP/Test.java
+++ src/edu/drexel/GOP/Test.java
@@ -2,7 +2,7 @@ package edu.drexel.GOP;
 
 public class Test {
 	public static void main(String[] args) {
-		GenericProxy proxy = new GenericProxy(4444, 5222);
+		MulticastProxy proxy = new MulticastProxy("228.5.6.7", 6789, 4444, 5222);
 		proxy.start();
 	}
 }
