4e2b8b98fc88c1e3f54b2a4fc44ac7c54bf19d17
lasalvavida <rht25@drexel.edu>
2013-05-23 14:09:50 -0400

    Added class for utility commands

---
+++ src/edu/drexel/GOP/GOPUtilities.java
@@ -0,0 +1,28 @@
+package edu.drexel.GOP;
+
+public final class GOPUtilities {
+	/**
+	 ** Converts an integer into an array of 4 bytes.
+	 ** Credit: http://stackoverflow.com/questions/6374915/java-convert-int-to-byte-array-of-4-bytes
+	 **/
+	public static final byte[] intToByteArray(int value) {
+		return new byte[] {
+			(byte)(value >>> 24),
+			(byte)(value >>> 16),
+			(byte)(value >>> 8),
+			(byte)value
+		};
+	}
+	/**
+	 ** Converts a byte array into an integer.
+	 ** Credit: http://stackoverflow.com/questions/5399798/byte-array-and-int-conversion-in-java
+	 **/
+	public static final int byteArrayToInt(byte[] b) {
+		int value = 0;
+		for (int i = 0; i < 4; i++) {
+			int shift = (4 - 1 - i) * 8;
+			value += (b[i] & 0x000000FF) << shift;
+		}
+		return value;
+	}
+}
